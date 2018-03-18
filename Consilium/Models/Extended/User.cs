f01896cb1421c6dcf76a4cbf5e8f43d48bb5f588
giovanniani <giovanni_ani@hotmail.com>
2018-03-18 14:26:45 -0600

    first commit

---
+++ Consilium/Models/Extended/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Consilium.Models
+{
+    public partial class User
+    {
+
+    }
+    public class UserMetadata
+    {
+        [Display(Name ="First name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailId { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string DateOfBirth { get; set; }
+
+
+    }
+
+}
